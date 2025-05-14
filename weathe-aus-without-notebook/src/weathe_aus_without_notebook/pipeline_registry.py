e6103e6372da1af8ca03c2580520f4730d70873f
Francisco <fra.castroa@duocuc.cl>
2025-05-13 22:19:43 -0400

    nodos nuevos

--- weathe-aus-without-notebook/src/weathe_aus_without_notebook/pipeline_registry.py
+++ weathe-aus-without-notebook/src/weathe_aus_without_notebook/pipeline_registry.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 # Corregir esta línea para usar el nombre real de tu paquete
-from weathe_aus_without_notebook.pipelines.data_to_postgres import pipeline as data_to_postgres
+from weathe_aus_without_notebook.pipelines import data_to_postgres, data_transform
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """
@@ -16,8 +16,10 @@ def register_pipelines() -> Dict[str, Pipeline]:
         Un diccionario con los pipelines.
     """
     data_to_postgres_pipeline = data_to_postgres.create_pipeline()
-
+    data_transform_pipeline = data_transform.create_pipeline()
+    
     return {
         "data_to_postgres": data_to_postgres_pipeline,
-        "__default__": data_to_postgres_pipeline,
+        "data_transform": data_transform_pipeline,
+        "__default__": data_to_postgres_pipeline + data_transform_pipeline,
     }
